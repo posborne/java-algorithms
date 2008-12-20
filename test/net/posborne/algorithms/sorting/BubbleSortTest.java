59acf00b4c00a1d3f41c47df590bce2ba33dc892
osbpau <paul-osborne@bethel.edu>
2008-12-19 22:55:45 -0600

    Added junit to classpath and added test package with bubble sort test class

---
+++ test/net/posborne/algorithms/sorting/BubbleSortTest.java
@@ -0,0 +1,14 @@
+package net.posborne.algorithms.sorting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BubbleSortTest {
+
+    @Test
+    public void testSort() {
+        fail("Not yet implemented");
+    }
+
+}
